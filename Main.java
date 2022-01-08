2669662d799279a2eedff17231c226c96ca3ebac
Augenweide-7 <user242968@163.com>
2022-01-08 20:43:35 +0800

    第一个java文件

---
+++ Main.java
@@ -0,0 +1,13 @@
+package dd.demo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        System.out.println(String.format("%8d %8d %8d",a,b,c));
+    }
+}
